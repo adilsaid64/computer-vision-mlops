197309559f9c811889fe8df3daf52c41eae43fd6
adil <adilsaid64@gmail.com>
2025-12-23 01:51:56 +0000

    feat: refactor into a main function, just practicing nvim tbh

--- simple/ingest-service/main.py
+++ simple/ingest-service/main.py
@@ -1,18 +1,22 @@
 import cv2
 
 RTSP_URL = "rtsp://localhost:8554/camera1?rtsp_transport=tcp"
-cap = cv2.VideoCapture(RTSP_URL, cv2.CAP_FFMPEG)
-if not cap.isOpened():
-    raise RuntimeError("Cannot open RTSP stream")
-print("Connected to RTSP stream")
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        print("Frame not received")
-        break
-    cv2.imshow("RTSP Stream", frame)
-    if cv2.waitKey(1) & 0xFF == ord("q"):
-        break
-cap.release()
-cv2.destroyAllWindows()
+def main():
+    cap = cv2.VideoCapture(RTSP_URL, cv2.CAP_FFMPEG)
+    if not cap.isOpened():
+        raise RuntimeError("Cannot open RTSP stream")
+    print("Connected to RTSP stream")
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            print("Frame not received")
+            break
+        cv2.imshow("RTSP Stream", frame)
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
 
+
+if __name__ == "__main__":
+    main()
