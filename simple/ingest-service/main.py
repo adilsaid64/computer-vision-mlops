312dca09ae5dc2e2da6431b90cf402b1acb45b5b
adil <adilsaid64@gmail.com>
2025-12-23 23:36:55 +0000

    feat: working get method

--- simple/ingest-service/main.py
+++ simple/ingest-service/main.py
@@ -1,22 +1,34 @@
 import cv2
+from fastapi import FastAPI
+from fastapi.responses import Response
+app = FastAPI()
 
 RTSP_URL = "rtsp://localhost:8554/camera1?rtsp_transport=tcp"
-def main():
-    cap = cv2.VideoCapture(RTSP_URL, cv2.CAP_FFMPEG)
-    if not cap.isOpened():
-        raise RuntimeError("Cannot open RTSP stream")
-    print("Connected to RTSP stream")
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            print("Frame not received")
-            break
-        cv2.imshow("RTSP Stream", frame)
-        if cv2.waitKey(1) & 0xFF == ord("q"):
-            break
-    cap.release()
-    cv2.destroyAllWindows()
 
+class RTSPClient:
+    def __init__(self, rtsp_url:str, wait_time:float):
+        self.rtsp_url = rtsp_url
+        self.wait_time = wait_time
+        self.cap = None
 
-if __name__ == "__main__":
-    main()
+    def connect(self):
+        self.cap = cv2.VideoCapture(self.rtsp_url, cv2.CAP_FFMPEG)
+        if self.cap.isOpened():
+            print('connected')
+        else:
+            raise RuntimeError('can not connected to rtsp stream')
+
+    def get_frame(self):
+        if self.cap:
+            ret, frame = self.cap.read()
+            sucess,encoded = cv2.imencode('.jpg', frame)
+            return encoded.tobytes()
+        else:
+            return None
+
+rtsp_client = RTSPClient(rtsp_url=RTSP_URL, wait_time=1)
+rtsp_client.connect()
+
+@app.get('/frame')
+def get_frame():
+    return Response(content = rtsp_client.get_frame(), media_type='image/jpeg')
